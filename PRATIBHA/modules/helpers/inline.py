7bb69903195ab151e49e179796bdec05b03e369c
🇸ᴀ ɴ ᴀ ᴛ ᴀ ɴ ɪ ₰ <157905660+SANATANI-EDITZ@users.noreply.github.com>
2024-04-29 16:59:03 +0530

    Add files via upload

---
+++ PRATIBHA/modules/helpers/inline.py
@@ -0,0 +1,102 @@
+from pyrogram.types import InlineKeyboardButton
+
+from config import SUPPORT_GRP, UPDATE_CHNL
+from PRATIBHA import BOT_USERNAME, OWNER
+
+DEV_OP = [
+    [
+        InlineKeyboardButton(text="• ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ •", url=f"https://t.me/PRATIBHA_ANIME_BOT?startgroup=true"),
+    ],
+    [
+        InlineKeyboardButton(text="• ᴜᴘᴅᴀᴛᴇ", url=f"https://t.me/ALL_SANATANI_BOT"),
+        InlineKeyboardButton(text="• sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/+_Bn9pPn9azVhMDg1"),
+    ],
+    [
+        InlineKeyboardButton(text="• ʜᴇʟᴘ ᴄᴏᴍᴍᴀɴᴅ •", callback_data="HELP"),
+    ],
+]
+
+####
+PNG_BTN = [
+    [
+        InlineKeyboardButton(text="• ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ •", url=f"https://t.me/PRATIBHA_ANIME_BOT?startgroup=true"),
+	],
+	[
+        InlineKeyboardButton(text="• ᴜᴘᴅᴀᴛᴇ •", url=f"https://t.me/ALL_SANATANI_BOT"),
+        InlineKeyboardButton(text="• sᴜᴘᴘᴏʀᴛ •", url=f"https://t.me/+_Bn9pPn9azVhMDg1"),
+    ],
+]
+
+#####
+BACK = [
+    [
+        InlineKeyboardButton(text="• ʙᴀᴄᴋ •", callback_data="BACK"),
+    ],
+]
+
+
+####
+HELP_BTN = [
+    [
+        InlineKeyboardButton(text="• ʙᴀᴄᴋ •", callback_data="BACK"),
+        InlineKeyboardButton(text="• ᴄʟᴏsᴇ •", callback_data="CLOSE"),
+    ],
+]
+
+#####
+CLOSE_BTN = [
+    [
+        InlineKeyboardButton(text="• ᴄʟᴏsᴇ •", callback_data="CLOSE"),
+    ],
+]
+
+####
+CHATBOT_ON = [
+    [
+        InlineKeyboardButton(text="ᴇɴᴀʙʟᴇ", callback_data=f"addchat"),
+        InlineKeyboardButton(text="ᴅɪsᴀʙʟᴇ", callback_data=f"rmchat"),
+    ],
+]
+
+#####
+CHATBOT_BACK = [
+    [
+        InlineKeyboardButton(text="• ʙᴀᴄᴋ •", callback_data="CHATBOT_BACK"),
+        InlineKeyboardButton(text="• ᴄʟᴏsᴇ •", callback_data="CLOSE"),
+    ],
+]
+
+####
+HELP_START = [
+    [
+        InlineKeyboardButton(text="• ʜᴇʟᴘ •", callback_data="HELP"),
+        InlineKeyboardButton(text="• ᴄʟᴏsᴇ •", callback_data="CLOSE"),
+    ],
+]
+
+#####
+HELP_BUTN = [
+    [
+        InlineKeyboardButton(text="• ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ •", url=f"https://t.me/PRATIBHA_ANIME_BOT?startgroup=true"),
+	],
+    [
+        InlineKeyboardButton(text="• ᴜᴘᴅᴀᴛᴇ •", url=f"https://t.me/ALL_SANATANI_BOT"),
+        InlineKeyboardButton(text="• sᴜᴘᴘᴏʀᴛ •", url=f"https://t.me/+_Bn9pPn9azVhMDg1"),
+    ],
+	[
+        InlineKeyboardButton(text="• ᴄʟᴏsᴇ •", callback_data="CLOSE"),
+    ],
+]
+
+######
+REPO = [
+    [
+        InlineKeyboardButton(text="• ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ •", url=f"https://t.me/PRATIBHA_ANIME_BOT?startgroup=true"),
+    ],
+    [
+        InlineKeyboardButton(text="• ᴜᴘᴅᴀᴛᴇ •", url=f"https://t.me/ALL_SANATANI_BOT"),
+        InlineKeyboardButton(text="• sᴜᴘᴘᴏʀᴛ •", url=f"https://t.me/+_Bn9pPn9azVhMDg1"),
+    ],
+]
+
+####
