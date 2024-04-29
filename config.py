66ea7586f466f61c80adf53a26499e8cd64f8c3a
🇸ᴀ ɴ ᴀ ᴛ ᴀ ɴ ɪ ₰ <157905660+SANATANI-EDITZ@users.noreply.github.com>
2024-04-29 16:55:47 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,88 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+#### ❖ ──────⊱◈◈◈⊰────── ❖
+API_ID = int(getenv("API_ID", "6435225"))
+
+#### ❖ ──────⊱◈◈◈⊰────── ❖
+API_HASH = getenv("API_HASH", "4e984ea35f854762dcde906dce426c2d")
+
+#### ❖ ──────⊱◈◈◈⊰────── ❖
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+
+#### ❖ ──────⊱◈◈◈⊰────── ❖
+OWNER_ID = int(getenv("OWNER_ID", "6922271843"))
+
+#### ❖ ──────⊱◈◈◈⊰────── ❖
+MONGO_URL = getenv("MONGO_URL", None)
+
+#### ❖ ──────⊱◈◈◈⊰────── ❖
+SUPPORT_GRP = getenv("SUPPORT_GRP", "THE_FRIENDZ")
+
+#### ❖ ──────⊱◈◈◈⊰────── ❖
+UPDATE_CHNL = getenv("UPDATE_CHNL", "ROY_EDITX")
+
+#### ❖ ──────⊱◈◈◈⊰────── ❖
+OWNER_USERNAME = getenv("OWNER_USERNAME", "HLO_PAPA")
+
+#### ❖ ──────⊱◈◈◈⊰────── ❖
+BOT_USERNAME = getenv("BOT_USERNAME", None)
+
+
+#### ❖ ──────⊱◈◈◈⊰────── ❖
+
+STICKER = [
+"CAACAgUAAx0Cd9xEawACEQlmLuvFijxhTZXjFbLPOsZFBoZzYQAC2AUAAkVZsVftrDRpvXZTAAE0BA",
+"CAACAgUAAx0Cd9xEawACEQhmLuvFeujHQdaxeDtr3MZThRXa1QACkgcAArZSuVfbJAABQq5pIc80BA",
+"CAACAgUAAx0Cd9xEawACEQdmLuvFQdwX-ySKIrmq-JPWItfhhgACwQUAAi9GuVfYV7lLP7xl4zQE",
+"CAACAgUAAx0Cd9xEawACEQVmLuuiUNrymw5wWSie-agvZ-_MdgACNAQAAi9GsFf3M2dSfxH-YDQE",
+"CAACAgUAAx0Cd9xEawACEQNmLuuKwCEUmunIPFoxUL1Kr2Dp1AAChQgAApAXsFeIwfQvrfbmjjQE",
+"CAACAgUAAx0Cd9xEawACEQJmLut22O_5LobAKvCBNlOHbCnQcQAC8gQAAmRQsVdeP26A2AJofzQE",
+"CAACAgUAAx0Cd9xEawACEQABZi7rYhnPjPsm_g37JvqoH7qB10gAAsgEAAJWgShXcBbC69nedAY0BA",
+"CAACAgUAAx0Cd9xEawACEP9mLutgBdWYCVPqQ_kvUGgYoNVIVwACrAYAAof0IFc6sUwgfJZw6zQE",
+"CAACAgEAAx0Cd9xEawACEPtmLusPo3kBvdEigRxbcqGOMSF9cgAC8wMAAqpT6UU55jSF8wAByTc0BA",
+"CAACAgEAAx0Cd9xEawACEPpmLusJTIEch-TXN5KsPkvdfnypNgACbwIAAkoY6UUP_O3RGOXeSTQE",
+"CAACAgEAAx0Cd9xEawACEPlmLusBSvWNswwz99iOXBMIos0s_QACGAMAAtfI6EX4deIoUongJDQE",
+"CAACAgEAAx0Cd9xEawACEPdmLuropCmTrN0Xv4_C7plvS45D3gACrwIAAqyx6EVOdFVb4d8VsDQE",
+"CAACAgUAAx0Cd9xEawACEOhmLurMc76ZYy9ZWB0dcuWfNJVSzQACLwUAAk-LuVelZAHYP-pxnTQE",
+"CAACAgUAAx0Cd9xEawACEOZmLuq8MMZnoz-txKJ9QEow9qDKxQACKwQAAvbXuVf7GDiuoypXFzQE",
+"CAACAgUAAx0Cd9xEawACEORmLuq3Mm3dzamR5W8JZhZHgbPWKwACJwcAAvQcsFefMIzhat8ZtDQE",
+"CAACAgUAAx0Cd9xEawACEONmLuqxMsLOLjCsMIf86_QuZH0AAaAAAusMAAIRzNhVUrENdULkjis0BA",
+"CAACAgUAAx0Cd9xEawACEOFmLuqryqMN4_7KPq_LLZNIq0OPEgACJAwAAm5mwVXkZ2Ycjy1rRjQE",
+"CAACAgUAAx0Cd9xEawACEN9mLuqlG8RAw-L8e1Pv3909WrYMhgACwBUAAh-sOVQ3vSSCUJbSYzQE",
+]
+
+#### ❖ ──────⊱◈◈◈⊰────── ❖
+
+IMG = [
+"https://graph.org/file/eaa3a2602e43844a488a5.jpg",
+"https://graph.org/file/b129e98b6e5c4db81c15f.jpg",
+"https://graph.org/file/3ccb86d7d62e8ee0a2e8b.jpg",
+"https://graph.org/file/df11d8257613418142063.jpg",
+"https://graph.org/file/9e23720fedc47259b6195.jpg",
+"https://graph.org/file/826485f2d7db6f09db8ed.jpg",
+"https://graph.org/file/ff3ad786da825b5205691.jpg",
+"https://graph.org/file/52713c9fe9253ae668f13.jpg",
+"https://graph.org/file/8f8516c86677a8c91bfb1.jpg",
+"https://graph.org/file/6603c3740378d3f7187da.jpg",
+"https://graph.org/file/66cb6ec40eea5c4670118.jpg",
+"https://graph.org/file/2e3cf4327b169b981055e.jpg",
+]
+
+#### ❖ ──────⊱◈◈◈⊰────── ❖
+
+EMOJIOS = [
+    "❤️",
+    "🧡",
+    "💛",
+    "💚",
+    "💙",
+    "💜",
+    "🤎",
+    "🖤",
+    "🤍",
+    "♥️",
+]
